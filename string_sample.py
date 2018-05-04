f0247e35193b2b8614483b4c526d99319060ca32
abdelkrim_daher <abdelkrim.daher@nuance.com>
2018-05-04 12:31:05 -0400

    first commit

---
+++ string_sample.py
@@ -0,0 +1,7 @@
+# Hello World program in Python
+    
+chaine = "Bonjour tout le monde!"
+print "Original: ", chaine
+print "Fonction upper()", chaine.upper()
+print "Fonction lower()", chaine.lower()
+print 
