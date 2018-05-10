3ed5ee1be3a4728df10cfa7faa2f07b3a48a8ccb
adaher15 <karim2bib@hotmail.com>
2018-05-10 19:42:59 -0400

    Update string_sample.py

--- string_sample.py
+++ string_sample.py
@@ -1,7 +1,22 @@
-# Hello World program in Python
-    
-chaine = "Bonjour tout le monde!"
-print "Original: ", chaine
-print "Fonction upper()", chaine.upper()
-print "Fonction lower()", chaine.lower()
-print 
+#Les chaines de carateres
+chaine="Bonjour tout le monde"
+print '\nOriginal: ', chaine
+print "\nFonction upper(): ", chaine.upper()
+print "\nFonction lower(): ", chaine.lower()
+#Operation sur les chaines de carateres
+chaine1='Boujour'
+chaine2='Lili'
+print "Adition de deux chaines", chaine1+" "+chaine2
+print "Multiplication de d'une chaines", chaine2*3
+# Bonjour Lili
+# BonjourLili
+
+#sous-chaine (substring)
+print "\n------",chaine,"------"
+l=len(chaine)
+print "Longueur d'une chaine avec fonction len(): ", l
+print "Position de 0 a 3", chaine[0:3]
+print "Position de 10 a len: ", chaine[10:l]
+print "Position de 10 a len: ", chaine[10]
+
+print "position -1: ", chaine  [-1]
