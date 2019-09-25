6f13dd2867acb1172286a9891cb87cd97a358763
adaher15 <karim2bib@hotmail.com>
2019-09-25 18:00:50 -0400

    Create algorithm_3.py

---
+++ algorithm_3.py
@@ -0,0 +1,41 @@
+r_1 = r_2 = g_1 = g_2 = y_1 = y_2 = False
+def change_light():
+     print("Function that will turn light on when value is True")
+
+#step 0
+r_1 = g_2 = True
+change_light()
+time.sleep(5)
+
+#step 1
+y_2 = True
+g_2 = False
+change_light()
+time.sleep(1)
+
+#step 2
+r_1 = y_2 = False
+g_1 = r_2 = True
+change_light()
+time.sleep(5)
+
+#step 4
+g_1 = False
+y_1 = True
+change_light()
+time.sleep(1)
+
+#step 5
+r_1 = g_2 = True
+y_1 = r_2 = False
+time.sleep(5)
+
+
+r_1 = r_2 = g_1 = g_2 = y_1 = y_2 = False
+def change_light():
+     print("....")
+
+while True:
+   #step 0
+ 
+ 
